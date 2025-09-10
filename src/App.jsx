86724dbf81b432224ba3885c70477e4334b149f5
Fabricio <fabro715@gmail.com>
2025-09-10 17:42:09 -0400

    design system fonts and colors added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,14 @@ function App() {
 
   return (
     <>
-      <p className='bg-red-600 text-green-400'>hello world</p>
+      <p className='bg-neutral-900 text-green-400 text-preset-1'>hello world</p>
+      <p className='bg-neutral-900 text-green-400 text-preset-2'>hello world</p>
+      <p className='bg-neutral-900 text-green-400 text-preset-3'>hello world</p>
+      <p className='bg-neutral-900 text-green-400 text-preset-4'>hello world</p>
+      <p className='bg-neutral-900 text-green-400 text-preset-5'>hello world</p>
+      <p className='bg-neutral-900 text-green-400 text-preset-6'>hello world</p>
+      <p className='bg-neutral-900 text-green-400 text-preset-7'>hello world</p>
+      <p className='bg-neutral-900 text-green-400 text-preset-8'>hello world</p>
     </>
   )
 }
