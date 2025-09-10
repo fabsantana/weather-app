7605abd32e775b3074c3a1733f3ef064b2887cb2
Fabricio <fabro715@gmail.com>
2025-09-10 11:50:27 -0400

    initital setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { React } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='bg-red-600 text-green-400'>hello world</p>
+    </>
+  )
+}
+
+export default App
